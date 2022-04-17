2ecf5f9a6de2c27139f63912f1d8a7f26796b48e
SiddhiMudras <82324351+SiddhiMudras@users.noreply.github.com>
2022-04-17 14:03:21 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,4 +1,15 @@
 if ("serviceWorker" in navigator) {
     // register service worker
-    navigator.serviceWorker.register("/service-worker.js");
+  navigator.serviceWorker.register("/service-worker.js")
+    .then((reg) => console.log("Service Worker supported",reg))
+    .catch((err) => console.log("Service Worker not supported",err))
+
   }
+if ("serviceWorker" in navigator) {
+    // register service worker
+  window.addEventListener('load', () =>{
+    navigator.serviceWorker.register("/service-worker.js")
+    .then((reg) => console.log("Service Worker registered",reg))
+    .catch((err) => console.log("Service Worker not registered",err))
+  })
+}
