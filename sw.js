a7d33520aff65bd14d8810479733c930950168ff
SiddhiMudras <82324351+SiddhiMudras@users.noreply.github.com>
2022-05-03 12:09:30 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+
+const cacheName = 'food';
+
+const cacheAssets = [
+  "index.html",
+  "hmepage.html",
+  "main.html",
+  "s3.jpg",
+  "d_l.gif",
+  "myscript.js",
+  "main.js",
+];
+
+self.addEventListener('install', evt =>
+{
+  console.log('Service Worker Installed');
+  evt.waitUntil(
+    caches
+    .open(cacheName)
+    .then(cache => {
+      console.log("Caching Files");
+      cache.addAll(cacheAssets);
+    })
+    .then(() => self.skipWaiting())
+  )
+});
+
+self.addEventListener('activate', evt =>
+{
+  console.log('Service Worker Activated');
+});
+
+self.addEventListener('fetch', evt =>
+{
+  console.log(evt.request.url);
+});
+self.addEventListener('push', evt =>
+{
+  console.log("Push Activated");
+});
