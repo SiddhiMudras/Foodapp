1372614ad78247b1f45dc032bf169ed203ed1d67
SiddhiMudras <82324351+SiddhiMudras@users.noreply.github.com>
2022-05-03 12:08:10 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
     // register service worker
-  navigator.serviceWorker.register("service-worker.js")
+  navigator.serviceWorker.register("sw.js")
     .then((reg) => console.log("Service Worker supported",reg))
     .catch((err) => console.log("Service Worker not supported",err))
 
@@ -8,8 +8,35 @@ if ("serviceWorker" in navigator) {
 if ("serviceWorker" in navigator) {
     // register service worker
   window.addEventListener('load', () =>{
-    navigator.serviceWorker.register("/service-worker.js")
+    navigator.serviceWorker.register("sw.js")
     .then((reg) => console.log("Service Worker registered",reg))
     .catch((err) => console.log("Service Worker not registered",err))
   })
+}
+
+if ('serviceWorker' in navigator && 'SyncManager' in window) {
+  navigator.serviceWorker.ready.then(function(reg) {
+    return reg.sync.register('first-sync');
+  }).catch(function() {
+    // system was unable to register for a sync,
+    // this could be an OS-level restriction
+    postDataFromThePage();
+  });
+} else {
+  // serviceworker/sync not supported
+  postDataFromThePage();
+}
+function push() {
+  //Subscribes user to Push notifications
+  registration.pushManager.subscribe({
+    userVisibleOnly: true //Set user to see every notification
+  })
+  .then(function (subscription) {
+    toast('Subscribed successfully.');
+    console.info('Push notification subscribed.');
+    console.log(subscription);
+  })
+  .catch(function (error) {
+    console.error('Push notification subscription error: ', error);
+  });
 }
